efb5de83846c798ea904a38d4ab1bd8d93a637ec
delaire thibault <thibault_d@outlook.fr>
2026-06-01 15:47:41 +0200

    controllers

---
+++ app/controllers/activity_sessions_controller.rb
@@ -0,0 +1,22 @@
+class ActivitySessionsController < ApplicationController
+  def new
+  end
+
+  def offer(id)
+  end
+
+  def choose
+  end
+
+  def regenerate
+  end
+
+  def activity
+  end
+
+  def done
+  end
+
+  def summary
+  end
+end
