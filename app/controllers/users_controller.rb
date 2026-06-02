24c92b3a507bdc383a469b5c4b3a360e3a4ea3ac
delaire thibault <thibault_d@outlook.fr>
2026-06-02 16:12:15 +0200

    cleaning de la user journey et du front

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,26 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def edit
+    @user = current_user
+    @avatars = User::AVATARS
+  end
+
+  def update
+    @user = current_user
+
+    if @user.update(user_params)
+      redirect_to user_path, notice: "Avatar mis à jour !"
+    else
+      @avatars = User::AVATARS
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:avatar)
+  end
 end
