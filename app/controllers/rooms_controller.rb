d00ae493a2f0fa0a85a0faff0e9bbbbe3aa48c96
delaire thibault <thibault_d@outlook.fr>
2026-06-05 11:43:31 +0200

    toutes les fonctions de room vont dans la show

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,6 +4,7 @@ class RoomsController < ApplicationController
   def show
     @room = current_user.room || current_user.create_room!(width: Room::GRID_WIDTH, height: Room::GRID_HEIGHT)
     @room.ensure_default_size!
+    @available_furnitures = Furniture.all
 
     @room_data = {
       id: @room.id,
@@ -14,7 +15,7 @@ class RoomsController < ApplicationController
           id: item.id,
           furniture_id: item.furniture_id,
           name: item.furniture.name,
-          image_url: item.furniture.image_url,
+          image_url: ActionController::Base.helpers.asset_path(item.furniture.image_url),
           width: item.furniture.width,
           height: item.furniture.height,
           x: item.x,
