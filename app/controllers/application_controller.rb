286e4d1775edf3474b116b993f1d7fa0e4fb2d7c
jc <jeanchristopheparfait@gmail.com>
2026-06-10 11:22:22 +0200

    Add Open Graph image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,15 @@ class ApplicationController < ActionController::Base
   # on exécute la méthode ci-dessous.
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   private
 
   # On autorise first_name et last_name à être enregistrés
   # UNIQUEMENT au moment de l'inscription (:sign_up).
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
   end
 end
